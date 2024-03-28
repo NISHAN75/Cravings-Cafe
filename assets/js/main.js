176d391d1f4d13cac8b0bb30014c1f6baacf08f0
NISHAN75 <nishandas880@gmail.com>
2024-03-28 18:12:26 +0600

    all is ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -198,6 +198,19 @@
                },
            },
        });
+       $(".blog-video-popup").magnificPopup({
+           type: "iframe",
+           iframe: {
+               markup: '<div class="mfp-iframe-scaler">' + '<div class="mfp-close"></div>' + '<iframe class="mfp-iframe" frameborder="0" allow="autoplay"></iframe>' + "</div>",
+               patterns: {
+                   youtube: {
+                       index: "youtube.com/",
+                       id: "v=",
+                       src: "https://www.youtube.com/embed/%id%?autoplay=1",
+                   },
+               },
+           },
+       });
        // Wow JS
        new WOW().init();
 
